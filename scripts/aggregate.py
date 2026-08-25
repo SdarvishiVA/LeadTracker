2727a17d692c5de51239e57544bd5f8dddc67266
SdarvishiVA <sdarvishi@vacapital.ca>
2026-08-25 16:21:30 -0400

    Update aggregate.py

--- scripts/aggregate.py
+++ scripts/aggregate.py
@@ -39,7 +39,7 @@ import openpyxl
 BROKERS = [
     {"broker": "Suzana", "box_file_id": "2412094141422"},
     {"broker": "Raph",   "box_file_id": "2415423203380"},
-    {"broker": "Kevin", "box_file_id": "2426484861293"},
+    {"broker": "Kevin",  "box_file_id": "2426484861293"},
     # {"broker": "NextBroker", "box_file_id": "PASTE_ID_HERE"},
 ]
 
